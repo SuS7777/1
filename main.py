393970acbe340f48c9fe1d872171b0a148a8b370
nshs31d-101 <olexandr.pasternak@gmail.com>
2022-09-03 10:20:07 +0300

    це мій перший коміт, він не створений для скаму, а просто для задоволення.

---
+++ main.py
@@ -0,0 +1,9 @@
+def name(string):
+    print(string)
+name("ОБЕРЕЖНО! СКАМ!")
+login = input('уведіть login???')
+password = input('уведіть password')
+print("Вас заскамила школота!")
+calc = (" ТЮБІК! " * 10)
+print(calc)
+print(len(calc))
